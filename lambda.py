9df20755b4e77c6ee7f66265b22897db663652cb
Prashanth Mamidi <prashanthm@cloudbuilders.io>
2024-07-24 15:55:46 +0530

    made small changes in lambda file

--- lambda.py
+++ lambda.py
@@ -1,1 +1,8 @@
-print("Hi")
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
